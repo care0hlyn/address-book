414685e9996277b8e14bc57c78bd4c5af14959ee
Carolyn Huynh & Justin Speers <student@epicodus.com>
2014-07-15 10:57:39 -0700

    Second commit

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  $('form#new-contact-form').submit(function(event){
+    event.preventDefault();
+
+    var newFirstName = $('#new-first-name').val();
+    var newLastName = $('#new-last-name').val();
+    var newAddress = $('#new-address').val();
+
+    var thisContact = { firstName: newFirstName,
+                        lastName: newLastName,
+                        address: newAddress };
+
+
+
+      $('#contact-list').append("<li><span class='contact'>" + thisContact.firstName + " " +
+                            thisContact.lastName + ", " +
+                            thisContact.address + "</span></li>");
+
+
+
+      $('.contact').last().click(function() {
+        $('#view-contact').show();
+        $('.first-name').text(thisContact.firstName);
+        $('.last-name').text(thisContact.lastName);
+        $('.address').text(thisContact.address);
+      });
+
+  });
+});
